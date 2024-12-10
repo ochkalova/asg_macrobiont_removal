32127e53ed76d01c80240625562b24deb9cbed48
Sofia Ochkalova <so.ochkalova@gmail.com>
2024-12-10 16:48:27 +0000

    remove unnecessary data and change output format to tsv

--- bin/metagenomes_data_import_mgnify.py
+++ bin/metagenomes_data_import_mgnify.py
@@ -1,11 +1,10 @@
 import requests
-import json
+import csv
 from lxml import etree
 
 ASG_URL = "https://portal.aquaticsymbiosisgenomics.org/api/data_portal_test"
 ENA_URL = "https://www.ebi.ac.uk/ena"
 RESULTS = []
-
 
 def main():
     offset = 0
@@ -25,8 +24,9 @@ def main():
         print(
             f"Processing data: {round(i / len(data) * 100, 2)}%\r", end="", flush=True
         )
-        if "assemblies" in record["_source"] and len(record["_source"]["assemblies"]) > 0:
-            RESULTS.append(collect_analyses_ids(record["_source"]))
+        record = record["_source"]
+        if ("assemblies" in record and len(record["assemblies"]) > 0) and ("metagenomes_assemblies" in record and len(record["metagenomes_assemblies"]) > 0):
+            RESULTS.append(collect_analyses_ids(record))
 
 
 def collect_analyses_ids(record):
@@ -36,39 +36,47 @@ def collect_analyses_ids(record):
     """
     tmp = dict()
     tmp["organism_name"] = record["organism"]
-    for assembly_type in [
-        "assemblies",
-        "metagenomes_assemblies",
-        "symbionts_assemblies",
-    ]:
-        tmp[assembly_type] = []
-        if assembly_type in record and len(record[assembly_type]) > 0:
-            for assmbl in record[assembly_type]:
-                tmp2 = {assmbl["accession"]: []}
-                response = requests.get(
-                    f"{ENA_URL}/browser/api/xml/{assmbl['accession']}"
-                )
-                root = etree.fromstring(response.content)
-                project_id = (
-                    root.find("ASSEMBLY")
-                    .find("STUDY_REF")
-                    .find("IDENTIFIERS")
-                    .find("PRIMARY_ID")
-                    .text
-                )
-                response = requests.get(
-                    f"https://www.ebi.ac.uk/ena/portal/api/search?result=analysis&"
-                    f"query=analysis_type=sequence_assembly%20AND%20assembly_type%3D%22primary%20metagenome%22%20AND%20study_accession%3D%22{project_id}%22&"
-                    f"format=json&fields=analysis_accession"
-                ).json()
-                
-                for erz in response:
-                    tmp2[assmbl["accession"]].append(erz["analysis_accession"])
-            tmp[assembly_type].append(tmp2)
+    tmp["root_organism_assembly"] = [assembly["accession"] for assembly in record["assemblies"]]
+    for assmbl in record["metagenomes_assemblies"]:
+        tmp2 = {assmbl["accession"]: []}
+        response = requests.get(
+            f"{ENA_URL}/browser/api/xml/{assmbl['accession']}"
+        )
+        root = etree.fromstring(response.content)
+        project_id = (
+            root.find("ASSEMBLY")
+            .find("STUDY_REF")
+            .find("IDENTIFIERS")
+            .find("PRIMARY_ID")
+            .text
+        )
+        response = requests.get(
+            f"https://www.ebi.ac.uk/ena/portal/api/search?result=analysis&"
+            f"query=analysis_type=sequence_assembly%20AND%20assembly_type%3D%22primary%20metagenome%22%20AND%20study_accession%3D%22{project_id}%22&"
+            f"format=json&fields=analysis_accession"
+        ).json()
+        
+        erz_list = [erz["analysis_accession"] for erz in response]
+        tmp2[assmbl["accession"]] = erz_list
+        tmp["metagenomes_assemblies"] = tmp2
     return tmp
+
+
+def write_tsv(output_file, results):
+    with open(output_file, "w", newline="") as tsvfile:
+        writer = csv.writer(tsvfile, delimiter='\t')
+        # Write the header
+        writer.writerow(["Organism Name", "Root organism Accession", "Metagenome Accessions"])
+        
+        # Write the data rows
+        for result in results:
+            organism_name = result["organism_name"]
+            root_genomes = ",".join(result["root_organism_assembly"])
+            metagenomes = ",".join([result["metagenomes_assemblies"][genome][0] for genome in result["metagenomes_assemblies"]])
+
+            writer.writerow([organism_name, root_genomes, metagenomes])
 
 
 if __name__ == "__main__":
     main()
-    with open("asg_mgnify_assemblies.json", "w") as outfile:
-        json.dump(RESULTS, outfile)
+    write_tsv("asg_mgnify_assemblies.tsv", RESULTS)
